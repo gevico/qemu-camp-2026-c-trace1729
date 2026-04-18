6b336b0cad8f0c29a46a3919e22ac491d3787342
gongkch <1106029170@qq.com>
2026-04-18 20:43:30 +0800

    commit 24

--- exercises/24_prev_binary_tree/simple_tree.c
+++ exercises/24_prev_binary_tree/simple_tree.c
@@ -10,14 +10,34 @@ Queue* create_queue() {
     return q;
 }
 
+// q->front
+// q->rear 指向下一个出队的元素
+
 void enqueue(Queue *q, TreeNode *tree_node) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    QueueNode* new_node = calloc(1, sizeof(QueueNode));
+    new_node->tree_node = tree_node;
+
+    if (q->front == NULL && q->rear == NULL) {
+        q->front = new_node;
+        q->rear = new_node;
+    } else {
+        q->front->next = new_node;
+        q->front = new_node;
+    }
+
+
 }
 
 TreeNode* dequeue(Queue *q) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (!q->rear) {
+        return NULL;
+    }
+    QueueNode* t = q->rear;
+    TreeNode* val = t->tree_node;
+    q->rear = q->rear->next;
+    if (q->rear == NULL) {q->front = NULL;}
+    free(t);
+    return val;
 }
 
 bool is_empty(Queue *q) {
@@ -32,18 +52,75 @@ void free_queue(Queue *q) {
 }
 
 TreeNode* build_tree_by_level(int *level_order, int size) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    TreeNode* root = calloc(1, sizeof(TreeNode));
+    root->val = level_order[0];
+
+    // initial state, transition conditon and termination condition
+    // general for loop contains only one state, if you have multiple pending state, you need queue.
+    Queue* pendig = create_queue();
+    enqueue(pendig, root);
+    // 层次遍历，idx 是自然递增的
+    int idx = 1;
+    while (!is_empty(pendig)) {
+        TreeNode* t = dequeue(pendig);
+        if (!t) {
+            printf("error!");
+            exit(-1);
+        }
+        if (idx < size && level_order[idx] != INT_MIN) {
+            TreeNode* left = calloc(1, sizeof(TreeNode));
+            left->val = level_order[idx];
+            t->left = left;
+            enqueue(pendig, left);
+        }
+        idx++;
+        if (idx < size && level_order[idx] != INT_MIN) {
+            TreeNode* right = calloc(1, sizeof(TreeNode));
+            right->val = level_order[idx];
+            t->right = right;
+            enqueue(pendig, right);
+        }
+        idx++;
+    }
+    return root;
 }
 
 void preorder_traversal(TreeNode *root) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (!root) {return ; }
+    printf("%d ", root->val);
+    preorder_traversal(root->left);
+    preorder_traversal(root->right);
 }
 
 void preorder_traversal_iterative(TreeNode *root) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (root == NULL) {
+        return;
+    }
+
+    QueueNode *stack = calloc(1, sizeof(QueueNode));
+    stack->tree_node = root;
+
+    while (stack != NULL) {
+        QueueNode *top = stack;
+        TreeNode *t = top->tree_node;
+        stack = stack->next;
+        free(top);
+
+        printf("%d ", t->val);
+
+        if (t->right != NULL) {
+            QueueNode *node = calloc(1, sizeof(QueueNode));
+            node->tree_node = t->right;
+            node->next = stack;
+            stack = node;
+        }
+        if (t->left != NULL) {
+            QueueNode *node = calloc(1, sizeof(QueueNode));
+            node->tree_node = t->left;
+            node->next = stack;
+            stack = node;
+        }
+    }
 }
 
 void free_tree(TreeNode *root) {
