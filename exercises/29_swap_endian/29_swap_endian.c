93a10ec7703e8dbecf2dad7cd46bf19a525edaac
gongkch <1106029170@qq.com>
2026-04-19 13:18:31 +0800

    commit 29-30

--- exercises/29_swap_endian/29_swap_endian.c
+++ exercises/29_swap_endian/29_swap_endian.c
@@ -2,9 +2,21 @@
 #include <stdio.h>
 
 uint32_t swap_endian(uint32_t num) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    // basically extract >> 1
+    // first extract lower bits num & 0x000000ff
+    // num & 0x0000ff00 >> 8
+    // num & 0xff000000 >> 8 * 2
+    uint32_t mask = 0x000000ff;
+    uint32_t res = 0;
+    for (int i = 0; i < 4; i++) {
+        uint8_t byte = (num & mask) >> (8*i);
+        res = (res << 8) + byte;
+        mask <<= 8;
+    }
+
+    return res;
 }
+
 
 int main(int argc, char* argv[]) {
     uint32_t num = 0x78563412;
--- exercises/30_debug_print/30_debug_print.c
+++ exercises/30_debug_print/30_debug_print.c
@@ -2,11 +2,32 @@
 #include <stdlib.h>
 #include <execinfo.h>
 
-#ifdef TODO
+#ifdef DEBUG_LEVEL
 
-#define DEBUG_PRINT(fmt, ...) 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+#if DEBUG_LEVEL >= 3
+#define DEBUG_PRINT(fmt, ...)                                                   \
+    do {                                                                        \
+        void *buffer[32];                                                       \
+        int nptrs;                                                              \
+        printf("DEBUG: func=%s, line=%d, " fmt "\n", __func__, __LINE__,        \
+               ##__VA_ARGS__);                                                  \
+        nptrs = backtrace(buffer, 32);                                          \
+        backtrace_symbols_fd(buffer, nptrs, 1);                                 \
+    } while (0)
+#elif DEBUG_LEVEL >= 2
+#define DEBUG_PRINT(fmt, ...)                                                   \
+    do {                                                                        \
+        printf("DEBUG: func=%s, line=%d, " fmt "\n", __func__, __LINE__,        \
+               ##__VA_ARGS__);                                                  \
+    } while (0)
+#elif DEBUG_LEVEL >= 1
+#define DEBUG_PRINT(fmt, ...)                                                   \
+    do {                                                                        \
+        printf("DEBUG: func=%s, line=%d\n", __func__, __LINE__);                \
+    } while (0)
+#else
+#define DEBUG_PRINT(fmt, ...) do {} while (0)
+#endif
 
 #else
 
@@ -42,6 +63,7 @@
 
 //! MUST BE ENSURE THE DEBUG_PRINT("x=%d", x) AT THE 48 LINE
 
+#line 45
 // 测试代码
 void test() {
     int x = 42;
