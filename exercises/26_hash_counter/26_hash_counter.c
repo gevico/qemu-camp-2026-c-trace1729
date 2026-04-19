3b49404e165165e7089e7858d2f3fc2385db8a67
gongkch <1106029170@qq.com>
2026-04-19 11:55:34 +0800

    commit 25-27

--- exercises/26_hash_counter/26_hash_counter.c
+++ exercises/26_hash_counter/26_hash_counter.c
@@ -20,8 +20,14 @@ typedef struct {
 
 // djb2哈希函数
 unsigned long djb2_hash(const char *str) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = (unsigned char)*str++) != 0) {
+        hash = ((hash << 5) + hash) + (unsigned long)c;
+    }
+
+    return hash;
 }
 
 // 创建哈希表
@@ -35,15 +41,35 @@ HashTable *create_hash_table(int size) {
 // 向哈希表中插入单词
 void hash_table_insert(HashTable *ht, const char *word) {
     unsigned long hash = djb2_hash(word) % ht->size;
+    HashNode *node = ht->table[hash];
 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    while (node != NULL) {
+        if (strcmp(node->word, word) == 0) {
+            node->count++;
+            return;
+        }
+        node = node->next;
+    }
+
+    node = malloc(sizeof(HashNode));
+    node->word = malloc(strlen(word) + 1);
+    strcpy(node->word, word);
+    node->count = 1;
+    node->next = ht->table[hash];
+    ht->table[hash] = node;
 }
 
 // 从哈希表中获取所有单词及其计数
 void get_all_words(HashTable *ht, HashNode **nodes, int *count) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    *count = 0;
+    for (int i = 0; i < ht->size; i++) {
+        HashNode *node = ht->table[i];
+        while (node != NULL) {
+            nodes[*count] = node;
+            (*count)++;
+            node = node->next;
+        }
+    }
 }
 
 // 比较函数用于排序
@@ -52,8 +78,10 @@ int compare_nodes(const void *a, const void *b) {
     HashNode *node_b = *(HashNode **)b;
     
     // 先按计数降序，再按字母升序
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (node_a->count != node_b->count) {
+        return node_b->count - node_a->count;
+    }
+    return strcmp(node_a->word, node_b->word);
 }
 
 // 释放哈希表内存
@@ -73,8 +101,32 @@ void free_hash_table(HashTable *ht) {
 
 // 从字符串中获取下一个单词
 char *get_next_word(const char **text) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    const char *p = *text;
+    int len = 0;
+
+    while (*p != '\0' && !isalpha((unsigned char)*p)) {
+        p++;
+    }
+
+    if (*p == '\0') {
+        *text = p;
+        return NULL;
+    }
+
+    const char *start = p;
+    while (isalpha((unsigned char)*p)) {
+        p++;
+        len++;
+    }
+
+    char *word = malloc((size_t)len + 1);
+    for (int i = 0; i < len; i++) {
+        word[i] = (char)tolower((unsigned char)start[i]);
+    }
+    word[len] = '\0';
+
+    *text = p;
+    return word;
 }
 
 int main(int argc, char *argv[]) {
