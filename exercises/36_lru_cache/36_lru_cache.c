859ef52847be564b989702d45f12fafbca6b4e8e
gongkch <1106029170@qq.com>
2026-04-20 18:14:34 +0800

    commit 36

--- exercises/36_lru_cache/36_lru_cache.c
+++ exercises/36_lru_cache/36_lru_cache.c
@@ -8,6 +8,16 @@
  *  - 容量满时淘汰最久未使用（LRU）的元素
  */
 
+#define DEBUG
+
+#define ASSERT(cond) \
+do { \
+    if (((cond) == 0))  { \
+        printf("assert func=%s, line=%d\n", __func__, __LINE__); \
+        exit(1);\
+    } \
+} while(0);
+
 typedef struct LRUNode {
     int key;
     int value;
@@ -31,64 +41,204 @@ typedef struct {
     HashEntry** buckets;
 } LRUCache;
 
-static unsigned hash_int(int key) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+static unsigned hash_int(LRUCache* c, int key) {
+    return key % (c->bucket_count);
 }
 
+/*
+ *  while (*link && (*link)->key != key) {
+ *     smarter way.
+ *      // link stores the handle of all the next ptr of the bucket list
+ *      link = &(*link)->next;
+    }
+    // when the loop end, link stores ptr to the "next" field that statisfy the next->key == key.
+ * */
+
+
+/*
+ *  The pointer are just wrapper around of how memory access a pointer can resolve to.
+ * */
 static HashEntry* hash_find(LRUCache* c, int key, HashEntry*** pprev_next) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    DEBUG
+    ASSERT(hash_int(c, key) < c->bucket_count);
+    HashEntry* head = c->buckets[hash_int(c, key)];
+    HashEntry* prev = head;
+    ASSERT(head != NULL);
+    head = head->next; // head is sential
+
+    while (head != NULL && head->node != NULL && head->node->key != key) {
+        prev = head;
+        head = head->next;
+    }
+    // if pprev_next is not NULL, the hashmap must contain the target.
+    if (pprev_next != NULL) {
+        ASSERT(prev != NULL);
+        *pprev_next = &(prev->next);
+    }
+
+    return head;
 }
 
 static void list_add_to_head(LRUCache* c, LRUNode* node) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    DEBUG
+    ASSERT(node != NULL);
+    node->next = c->head->next;
+    c->head->next = node;
+    node->next->prev = node;
+    node->prev = c->head;
 }
 
 static void list_remove(LRUCache* c, LRUNode* node) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    DEBUG
+    ASSERT(node != NULL);
+    LRUNode* p = c->head;
+    while (p != c->tail && p != node) {p = p->next;}
+    if (p == c->tail) {return;}
+    ASSERT(node->prev);
+    ASSERT(node->next);
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    free(node);
 }
 
 static void list_move_to_head(LRUCache* c, LRUNode* node) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    DEBUG
+    ASSERT(node != NULL);
+    LRUNode* p = c->head;
+    while (p != c->tail && p != node) {p = p->next;}
+    if (p == c->tail) {return;}
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    list_add_to_head(c, node);
 }
 
 static LRUNode* list_pop_tail(LRUCache* c) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    DEBUG
+    ASSERT(c->tail != NULL);
+    if (c->tail->prev == c->head) {return NULL;}
+    LRUNode* evict = c->tail->prev;
+    ASSERT(c->tail->prev != NULL);
+    ASSERT(c->tail->prev->prev != NULL);
+    c->tail->prev->prev->next = c->tail;
+    c->tail->prev = c->tail->prev->prev;
+    return evict;
 }
 
 /* LRU 接口实现 */
 static LRUCache* lru_create(int capacity) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    DEBUG
+    // 0. inint lrucache
+    LRUCache* lru = calloc(1, sizeof(LRUCache));
+    lru->capacity = capacity;
+    // 1. init list
+    lru->head = calloc(1, sizeof(LRUNode));
+    lru->tail = calloc(1, sizeof(LRUNode));
+    lru->head->next = lru->tail;
+    lru->tail->prev = lru->head;
+    // 2. init hashmap
+    lru->buckets = calloc(capacity, sizeof(HashEntry*));
+    for (int i = 0; i < capacity; i++) {
+        lru->buckets[i] = calloc(1, sizeof(HashEntry));
+    }
+    lru->bucket_count = capacity;
+    return lru;
 }
 
 static void lru_free(LRUCache* c) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    DEBUG
+    // free_list
+    ASSERT(c->head != NULL && c->tail != NULL);
+    LRUNode* p = c->head;
+    while(p != c->tail) {
+        LRUNode* t = p;
+        p = p->next;
+        free(t);
+    }
+    free(c->tail);
+
+    // free hash_map
+    for (size_t i = 0; i < c->bucket_count; i++) {
+        HashEntry* head = c->buckets[i];
+        while (head != NULL) {
+            HashEntry* t = head;
+            head = head->next;
+            free(t);
+        }
+    }
+    // free lru
+    free(c);
 }
 
 static int lru_get(LRUCache* c, int key, int* out_value) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    DEBUG
+    HashEntry* n = hash_find(c, key, NULL);
+    if (n == NULL) {
+        return -1;
+    }
+    ASSERT(n->node != NULL);
+    *out_value = n->node->value;
+    list_move_to_head(c, n->node);
+    return 0;
+}
+
+static void hash_insert(LRUCache* c, int key, LRUNode* data) {
+    DEBUG
+    ASSERT(data != NULL);
+    HashEntry* entry = calloc(1, sizeof(HashEntry));
+    entry->key = key;
+    entry->node = data;
+    int idx = hash_int(c, entry->key);
+    HashEntry* bucket_head = c->buckets[idx];
+    entry->next = bucket_head->next;
+    bucket_head->next = entry;
+
+}
+
+static void lru_evict(LRUCache* c) {
+    DEBUG
+    // link_list remove
+    LRUNode* evict = list_pop_tail(c);
+    ASSERT(evict != NULL);
+    // hash_remove
+    HashEntry** pprev_next = NULL; // "generlize" the op which find the ptr that points to the node before the target node;
+    HashEntry* n = hash_find(c, evict->key, &pprev_next);
+    ASSERT(pprev_next != NULL);
+    *pprev_next = n->next;
+    free(n);
+    free(evict);
+    c->size--;
 }
 
 static void lru_put(LRUCache* c, int key, int value) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    DEBUG
+
+    HashEntry* n = hash_find(c, key, NULL);
+    if (n != NULL) {
+        list_move_to_head(c, n->node);
+    } else {
+        if (c->size + 1 > c->capacity) {
+            lru_evict(c);
+        }
+        // link_list insert
+        LRUNode* data = calloc(1, sizeof(LRUNode));
+        data->key = key, data->value = value;
+        list_add_to_head(c, data);
+        // hash_map insert
+        hash_insert(c, key, data);
+        c->size++;
+    }
 }
 
+
 /* 打印当前缓存内容（从头到尾） */
-static void lru_print(LRUCache* c) {
-    LRUNode* p = c->head;
-    int first = 1;
-    while (p) {
-        if (!first) printf(", ");
-        first = 0;
+static void lru_print(LRUCache* lru) {
+    DEBUG
+    ASSERT(lru->head != NULL && lru->tail != NULL);
+    LRUNode* p = lru->head->next;
+    int flag = 1;
+    while(p != lru->tail) {
+        if (!flag) printf(", ");
+        flag = 0;
         printf("%d:%d", p->key, p->value);
         p = p->next;
     }
@@ -98,6 +248,7 @@ static void lru_print(LRUCache* c) {
 int main(void) {
     /* 容量 3：put(1,1), put(2,2), put(3,3), put(4,4), get(2), put(5,5) */
     LRUCache* c = lru_create(3);
+    DEBUG
     if (!c) {
         fprintf(stderr, "创建 LRU 失败\n");
         return 1;
